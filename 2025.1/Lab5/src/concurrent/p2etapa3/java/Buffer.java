3590b3a020425dc54dc4477956ec7cc49bcd9448
mttgermano <mttgermano@proton.me>
2025-07-29 16:14:09 -0300

    etapa3

---
+++ 2025.1/Lab5/src/concurrent/p2etapa3/java/Buffer.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Buffer {
+    private final List<Integer> data = new ArrayList<>();
+    private final int TAMANHO = 10;
+    
+    public synchronized void put(int value) {
+        while(data.size() == TAMANHO){
+            try{
+                wait();
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+        data.add(value);
+        System.out.println("Inserted: " + value + " | Buffer size: " + data.size());
+        notifyAll();
+    }
+    
+    public synchronized int remove() {
+        while (data.isEmpty()) {
+            try{
+                wait();
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+            int value = data.remove(0);
+            System.out.println("Removed: " + value + " | Buffer size: " + data.size());
+            notifyAll();
+            return value;
+        }
+        return -1;
+    }
+}
