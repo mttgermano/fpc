424dab5953882e1172dfaf1bebf4f92423dc8a83
mttgermano <mttgermano@proton.me>
2025-07-29 16:04:44 -0300

    etapa1

---
+++ 2025.1/Lab5/src/concurrent/p2etapa1/java/Consumer.java
@@ -0,0 +1,29 @@
+class Consumer implements Runnable{
+    private final Buffer buffer;
+    private final int sleepTime;
+    private final int id;
+    
+    public Consumer(int id, Buffer buffer, int sleepTime) {
+        this.id = id;
+        this.buffer = buffer;
+        this.sleepTime = sleepTime;
+    }
+    
+    public void process() {
+        while (true) {
+            int item = buffer.remove();
+            if (item == -1) break;
+            System.out.println("Consumer " + id + " consumed item " + item);
+            try {
+                Thread.sleep(sleepTime);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+    }
+
+    @Override 
+    public void run(){
+        process();
+    }
+}
